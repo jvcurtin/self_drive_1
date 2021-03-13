a9f80d133f204aa25048e2d975815d643de4cf6d
jvcurtin <80599840+jvcurtin@users.noreply.github.com>
2021-03-13 14:03:23 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.pause(1000)
+basic.forever(function () {
+    while (maqueen.Ultrasonic(PingUnit.Centimeters) > 15) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+        while (maqueen.Ultrasonic(PingUnit.Centimeters) <= 20) {
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 120)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 30)
+        }
+    }
+})
